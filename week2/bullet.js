0156f968137fadd49e159a43f928ba5b2a46898a
Jim Cramer <remarcmij@gmail.com>
2021-11-27 01:48:23 +0100

    More examples

---
+++ week2/bullet.js
@@ -0,0 +1,23 @@
+function fireBullet() {
+  return new Promise((resolve, reject) => {
+    for (distance = 0; distance < 10; distance++) {
+      console.log('distance', distance);
+      if (distance === 5) {
+        console.log(`--> calling resolve at distance ${distance}`);
+        resolve(`>>> Bullet hit target at distance ${distance}`);
+      }
+      if (distance === 7) {
+        console.log(`--> calling reject at distance ${distance}`);
+        reject(`>>> Bullet is out of range at distance ${distance}`);
+      }
+    }
+  });
+}
+
+fireBullet()
+  .then((message) => {
+    console.log('Resolved', message);
+  })
+  .catch((error) => {
+    console.log('Rejected', error);
+  });
